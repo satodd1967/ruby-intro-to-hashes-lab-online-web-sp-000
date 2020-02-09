cb1b3210c1e11ec659a667dea57d01b3f3a1a530
Seth Todd <seth.todd@outlook.com>
2020-02-09 23:01:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {:railroads => {:pieces => 4,}}
+	
 
 end
 
 def monopoly_with_fourth_tier
-  monopoly = {:railroads => {:pieces => 4, :names => {}}}
+  
 
 end
